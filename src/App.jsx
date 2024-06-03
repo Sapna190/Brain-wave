2794493e668dac744888f95dc30a4f07289f3012
Sapna190 <sapnasharma8849711@gmail.com>
2024-06-03 23:17:47 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
     
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header/>
       </div>
       <ButtonGradient/>
