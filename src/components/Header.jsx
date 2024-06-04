28d6546fc3de0a6e17a8ab6a91475c9d84be203b
Sapna190 <sapnasharma8849711@gmail.com>
2024-06-04 22:50:35 +0530

    added components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { brainwave } from "../assets";
 
 const Header = () => {
   return (
+    <>
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm 
     border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
     <div className="flex intems-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"></div>
@@ -9,6 +10,10 @@ const Header = () => {
         <image src={brainwave} width={190} height={40} /> 
      </a>
     </div>
+    <div>
+      <button>submit</button>
+    </div>
+    </>
   )
 }
 
